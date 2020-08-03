6a67aca679d752d50ea0d1e923a017ea3f30e565
savitoh <savioraires10@gmail.com>
2020-08-02 22:23:46 -0300

    Adição desafio 12.

---
+++ java-12/src/main/java/com/challenge/segurity/service/UserPrincipalDetailsService.java
@@ -0,0 +1,26 @@
+package com.challenge.segurity.service;
+
+import com.challenge.entity.User;
+import com.challenge.repository.UserRepository;
+import com.challenge.segurity.UserPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService  {
+
+    private final UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        final User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Não existe usuario com email: " + email));
+        return new UserPrincipal(user);
+    }
+}
